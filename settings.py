946849f112ca39dc194ed6fd8eae97a00bd0f9d0
akshajdwivedula <62357669+akshajdwivedula@users.noreply.github.com>
2020-03-27 09:21:54 -0700

    First week change for GameQuest

--- settings.py
+++ settings.py
@@ -1,13 +1,19 @@
-###This file was created by: Akshaj Dwivedula
+# game options/settings
+TITLE = "Jumpy!"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+#Environment options
+GRAVITY = 9.8
 
-# global variables
-RUNNING = True
-# screen dims
-WIDTH = 805
-HEIGHT = 605
-# frames per second
-FPS = 30
-# colors
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+
+# define colors
 WHITE = (255, 255, 255)
-BLACK = (0,0,0)
-REDDISH = (240,55,66)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
--- sprites.py
+++ sprites.py
@@ -1,141 +1,66 @@
-##This file was created by: Akshaj Dwivedula
-
-import pygame
+  
+# Sprite classes for platform game
+import pygame as pg
 from pygame.sprite import Sprite
 from settings import *
-
+vec = pg.math.Vector2
 
 class Player(Sprite):
-    # sprite for player
-    # properties of the class
-    def __init__(self):
+    def __init__(self, game):
         Sprite.__init__(self)
-        self.image = pygame.Surface((50,50))
-        self.image.fill(BLACK)
-        '''sets image path to correct location joining image folder to file name then converting to a more efficient format'''
-        # self.image = pygame.image.load(os.path.join(img_folder, "Tie.png")).convert()
-        '''sets transparent color key to black'''
-        # self.image.set_colorkey(BLACK)
+        self.game = game
+        self.image = pg.Surface((30, 45))
+        self.image.fill(BLUE)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        # self.screen_rect = screen.get_rect()
-        self.vx = 0
-        self.vy = 0
-        self.cofric = 0.5
-        self.canjump = False
-    # stuff it can do....
-    def friction(self):
-        self.cofric = .25
-        if self.vx > 0.5:
-            self.vx -= self.cofric
-        elif self.vx < -0.5:
-            self.vx += self.cofric
-        else:
-            self.vx = 0
-        if self.vy < -0.5:
-            self.vy += self.cofric
-        elif self.vy > 0.5:
-            self.vy -= self.cofric
-        else:
-            self.vy = 0
+        self.pos = vec(WIDTH / 2, HEIGHT / 2)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+    def myMethod(self):
+        pass
     def jump(self):
-        if self.canjump == True:
-            self.canjump = False
-            self.vy -= 50
-    def gravity(self, value):
-        self.vy += value
+        self.rect.x += 1
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        self.rect.x -= -1
+        if hits:
+            self.vel.y = -40
     def update(self):
-        self.friction()
-        if self.rect.bottom < HEIGHT:
-            self.gravity(9.8)
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
-            self.vy = 0
-            self.canjump = True
-        if self.rect.top < 0:
-            self.rect.top = 0
-            self.vy = 0
-        self.rect.x += self.vx
-        self.rect.y += self.vy
-        # if self.rect.right > 790:
-        #     self.vx = -2
-        # if self.rect.right < 10:
-        #     self.vx = 2
-        # if self.rect.top > 590:
-        #     self.vy = -2
-        # if self.rect.top < 10:
-        #     self.vy = 2
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_w]:
-            self.vy -= 1
-        if keystate[pygame.K_a]:
-            self.vx -= 1
-        if keystate[pygame.K_s]:
-            self.vy += 1
-        if keystate[pygame.K_d]:
-            self.vx += 1
-        if keystate[pygame.K_x]:
-            self.vx = 0 
-            self.vy = 0
-        if keystate[pygame.K_SPACE]:
+        self.acc = vec(0, 0.5)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.acc.x = -PLAYER_ACC
+        if keys[pg.K_RIGHT]:
+            self.acc.x = PLAYER_ACC
+        if keys[pg.K_UP]:
+            self.acc.y = -PLAYER_ACC
+        if keys[pg.K_DOWN]:
+            self.acc.y = PLAYER_ACC
+        if keys[pg.K_SPACE]:
             self.jump()
-        # if keystate[pygame.K_SPACE]:
-        #     self.shoot()
-        # if self.rect.right > WIDTH:
-        #     self.rect.right = WIDTH
-        # if self.rect.left < 0:
-        #     self.rect.left = 0
-        # if self.rect.bottom > HEIGHT:
-        #     self.rect.bottom = HEIGHT
-        # if self.rect.top < 0:
-        #     self.rect.top = 0
-    
-    # def hitWall():
 
+        # apply friction
+        self.acc += self.vel * PLAYER_FRICTION
+        # equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        # wrap around the sides of the screen
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+        if self.pos.y < 0:
+            self.pos.y = HEIGHT
+        if self.pos.y > HEIGHT:
+            self.pos.y = 0
 
-class Enemy(Sprite):
-    # sprite for enemy
-    # properties of the class
-    def __init__(self):
+        self.rect.center = self.pos
+
+class Platform(Sprite):
+    def __init__(self, x, y, w, h):
         Sprite.__init__(self)
-        #makes enemy different color and a little smaller than the player
-        self.image = pygame.Surface((35,35))
-        self.image.fill(WHITE)
-        '''sets image path to correct location joining image folder to file name then converting to a more efficient format'''
-        # self.image = pygame.image.load(os.path.join(img_folder, "Tie.png")).convert()
-        '''sets transparent color key to black'''
-        # self.image.set_colorkey(BLACK)
+        self.image = pg.Surface((w,h))
+        self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        # self.screen_rect = screen.get_rect()
-        self.vx = -3
-        self.vy = -2
-        self.cofric = 0.5
-    # stuff it can do....
-    def update(self):
-        self.rect.x += self.vx
-        self.rect.y += self.vy
-        if self.rect.right > 800:
-            self.vx = -3
-        if self.rect.left < 0:
-            self.vx = 3
-        if self.rect.top > 600:
-            self.vy = -2
-        if self.rect.bottom < 0:
-            self.vy = 2
-        # if keystate[pygame.K_SPACE]:
-        #     self.shoot()
-        # if self.rect.right > WIDTH:
-        #     self.rect.right = WIDTH
-        # if self.rect.left < 0:
-        #     self.rect.left = 0
-        # if self.rect.bottom > HEIGHT:
-        #     self.rect.bottom = HEIGHT
-        # if self.rect.top < 0:
-        #     self.rect.top = 0
-    # def hitWall():
-# init pygame and create window
+        self.rect.x = x
+        self.rect.y = y
+
